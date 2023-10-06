96eefef6582f33bef1f35a3d29903c04f559d410
Ayo <71637498+AyomideOladele@users.noreply.github.com>
2023-10-06 16:31:36 +0100

    Update CameraController.cs
    Changed Update to LateUpdate

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System . Collections . Generic ;
+using UnityEngine ;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    private Vector3 offset;
+
     void Start()
     {
-        
+        offset = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+       transform.position = player.transform.position + offset;
     }
 }
+
+
+/*
+    Code explanation from Lab 2
+
+   - A public game object, the player, target of the camera.
+   - A private offset, from the player to the camera.
+   - We use Start() to initialize the offset.This takes the initial position of the camera (note that we can use
+     this initialization because originally the player is at the origin).
+   - We modify the position of the camera (through its transform, directly accessible through the variable
+     transform), by adding the offset to the position of the player.
+   - We are doing this on the LateUpdate() method.LateUpdate() is the appropriate place for follow cameras,
+     procedural animations and gathering last known states.
+*/
